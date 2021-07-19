3ea50b147dba82091a1f0ccb61852e24d40f403f
gowrinair <55692062+gowrinair@users.noreply.github.com>
2021-07-19 15:56:31 +0530

    Update Simple.java
    new print statement

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
  class Simple{
      public static void main(String args[]){
         System.out.println("Hello Java");
-        System.out.println("new edit from github");
+        System.out.println("new edit from the repo of github!");
     }
 }
 
